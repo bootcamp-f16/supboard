a42fb7a44a57994fc1ee7e20b2de808b58a5b0f9
Andrew House <andrewhouse621@gmail.com>
2016-10-31 11:01:15 -0400

    Elastic Beanstalk Setup

--- conf/settings/production.py
+++ conf/settings/production.py
@@ -1,7 +1,16 @@
 from .base import *
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
